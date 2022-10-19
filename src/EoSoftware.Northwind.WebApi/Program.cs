e89bc5198aa5f8d08ce6f3c49b2004aad2a7782b
Emmett OSullivan <emmett.j.osullivan@gmail.com>
2022-10-19 17:31:22 +0100

    Updated to read ConnectionString from appsettings.json.

--- src/EoSoftware.Northwind.WebApi/Program.cs
+++ src/EoSoftware.Northwind.WebApi/Program.cs
@@ -1,6 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using EoSoftware.Northwind.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<NorthwindDbContext>(options => 
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("NorthwindDbContext"));
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
