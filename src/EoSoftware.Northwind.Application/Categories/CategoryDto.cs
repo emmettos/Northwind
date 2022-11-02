7bd4749ce5892c5c30bc7fbfc2c000770c30b80c
Emmett OSullivan <emmett.j.osullivan@gmail.com>
2022-11-02 15:37:43 +0000

    Added model validation attributes for CategoryDto.

--- src/EoSoftware.Northwind.Application/Categories/CategoryDto.cs
+++ src/EoSoftware.Northwind.Application/Categories/CategoryDto.cs
@@ -1,8 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace EoSoftware.Northwind.Application;
 
 public class NewCategoryDto
 {
+    [Required]
+    [StringLength(15, MinimumLength = 3)]
     public string Name { get; set; } = null!;
+    [Required]
     public string? Description { get; set; }
 }
 
